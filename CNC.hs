c1ca9bbeb06d5162cd77aff0a435db0b71667e00
Henry Shepperd <hshepperd@gmail.com>
2019-03-19 15:19:57 +1100

    Add MoveTo command, and slightly more reliable receive function

--- CNC.hs
+++ CNC.hs
@@ -63,6 +63,7 @@ data Command
     = Ping
     | GetPosition
     | SetPosition Vector
+    | MoveTo Vector
     deriving (Show)
 
 instance Binary Command where
@@ -79,6 +80,11 @@ instance Binary Command where
         put (69 :: Word8)
         put (2 :: Word8)
         put vec
+    put (MoveTo vec) = do
+        put (69 :: Word8)
+        put (69 :: Word8)
+        put (3 :: Word8)
+        put vec
     get = undefined
 
 ping :: Machine -> IO Bool
@@ -87,9 +93,8 @@ ping machine = ping' machine 10
         ping' machine 0 = return False
         ping' machine count = withSerial (serialPort machine) mySerialPortSettings $ \ s -> do
             send s $ LB.toStrict $ encode Ping
-            usleep 100000 -- This is a terrible thing to rely on.
-            r <- recv s 1000
-            if (r == B.pack [69]) then
+            r <- receive s 3
+            if (r == B.pack [69, 69, 0]) then
                 return True
             else
                 ping' machine (count - 1)
@@ -97,9 +102,13 @@ ping machine = ping' machine 10
 getPosition :: Machine -> IO (Maybe Vector)
 getPosition machine = withSerial (serialPort machine) mySerialPortSettings $ \ s -> do
     send s $ LB.toStrict $ encode GetPosition
-    usleep 100000 -- This is a terrible thing to rely on.
-    r <- recv s 100
-    return $ Just $ decode $ LB.fromStrict r
+    header <- receive s 3
+    if (header == B.pack [69, 69, 1]) then do
+        vect <- receive s (3 * 4)
+        return $ Just $ decode $ LB.fromStrict vect
+    else
+        return Nothing
+
 
 flushSerial machine = withSerial (serialPort machine) mySerialPortSettings $ \ s -> do
     r <- recv s 1000
@@ -108,17 +117,35 @@ flushSerial machine = withSerial (serialPort machine) mySerialPortSettings $ \ s
 setPosition :: Machine -> Vector -> IO Bool
 setPosition machine position = withSerial (serialPort machine) mySerialPortSettings $ \ s -> do
     send s $ LB.toStrict $ encode $ SetPosition position
-    usleep 100000 -- This is a terrible thing to rely on.
-    return True
+    header <- receive s 3
+    return (header == B.pack [69, 69, 2])
 
 -- move :: Vector -> IO Bool
 -- move = undefined
 
 moveTo :: Machine -> Vector -> IO Bool
-moveTo = undefined
+moveTo machine position = withSerial (serialPort machine) mySerialPortSettings $ \ s -> do
+    send s $ LB.toStrict $ encode $ MoveTo position
+    header <- receive s 3
+    return (header == B.pack [69, 69, 3])
 
 line :: Machine -> Vector -> IO Bool
 line = undefined
 
 circle :: Machine -> Vector -> Direction -> IO Bool
 circle = undefined
+
+-- Like recv, but tries again if it doesn't get enough bytes first try.
+-- Useful because sometimes recv doesn't get data of 35 bytes or more consistently.
+-- TODO Work out if recv "should" be working ok. Maybe the microcontroller is pausing
+--  unexpectedly during the serial transmission.
+receive :: SerialPort -> Int -> IO B.ByteString
+receive s n = do
+    a <- recv s n
+
+    if (B.length a) == n then
+        return a
+    else do
+        usleep 100000
+        b <- recv s n
+        return (B.append a b)
